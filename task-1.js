3c16e7b8b3eb97cbe58c5638ddfc996309f0f1d3
khalid09dev <mdkhalidhossen@hotmail.com>
2024-03-01 18:49:07 +0600

    added task-5 solution

--- task-1.js
+++ task-1.js
@@ -46,4 +46,21 @@ function findAveragePhonePrice(phones) {
 const avg = findAveragePhonePrice(phones);
 // console.log(Math.round(avg));
 
+// task-5
+const employees = [
+    { name: "shahin", experience: 5, starting: 20000, increment: 5000 },
+    { name: "shihab", experience: 3, starting: 15000, increment: 7000 },
+    { name: "shikot", experience: 9, starting: 30000, increment: 1000 },
+    { name: "shohel", experience: 0, starting: 29000, increment: 4000 },
+];
 
+function salary(employees) {
+    let salarySum = 0;
+    for(const employee of employees) {
+        const pay = employee.experience * employee.increment + employee.starting;
+        salarySum = salarySum + pay;
+    }
+    return salarySum;
+}
+const employeesBill = salary(employees);
+console.log('Monthly_Employees_Bill:',employeesBill);
