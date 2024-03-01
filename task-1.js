d90156b756e1b2df8eba807ca4d3d72768533ef9
khalid09dev <mdkhalidhossen@hotmail.com>
2024-03-01 17:47:36 +0600

    added problem-4 solution

--- task-1.js
+++ task-1.js
@@ -22,3 +22,28 @@ const budget = calculateElectronicsBudget(35000, 15000, 20000);
 // console.log(budget);
 
 // task-4
+const phones = [
+    { model: "PhoneA", brand: "Iphone", price: 95000 },
+    { model: "PhoneB", brand: "Samsung", price: 40000 },
+    { model: "PhoneC", brand: "Oppo", price: 26000 },
+    { model: "PhoneD", brand: "Nokia", price: 35000 },
+    { model: "PhoneE", brand: "Iphone", price: 105000 },
+    { model: "PhoneF", brand: "HTC", price: 48000 },
+];
+
+function findAveragePhonePrice(phones) {
+    let sum = 0;
+    let i = 0;
+    for(const phone of phones) {
+        sum = sum + phone.price;
+        if(i < phone.price) {
+            i++
+        }
+    }
+    const avg = sum / i;
+    return avg;
+}
+const avg = findAveragePhonePrice(phones);
+// console.log(Math.round(avg));
+
+
